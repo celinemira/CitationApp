3fe8d696c805e1f270f692f13be09e5fb32cd7e2
celinemira <celinemira.pro@gmail.com>
2024-06-25 10:52:00 +0200

    Citation application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CreateCitationComponent } from './create-citation/create-citation.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomepageComponent,CreateCitationComponent, FormsModule, NgModule, BrowserModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
 export class AppComponent {
   title = 'CitationApp';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CreateCitationComponent } from './create-citation/create-citation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component:HomepageComponent},
+    {path:'citation', component:CreateCitationComponent}
+];
